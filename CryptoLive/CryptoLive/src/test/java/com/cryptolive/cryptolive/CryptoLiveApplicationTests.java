fe17192c0c5bcc1f2ed30a7b07571b299855816a
Nahaza2425 <nabidhassan34@gmail.com>
2025-05-29 21:23:03 +0200

    Proyecto creado

---
+++ CryptoLive/CryptoLive/src/test/java/com/cryptolive/cryptolive/CryptoLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptolive.cryptolive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
