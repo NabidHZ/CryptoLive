8894cea00e1e8d2f658ff6b5f3b8b865e8208079
Nahaza2425 <nabidhassan34@gmail.com>
2025-05-29 21:47:56 +0200

    Packetes creados

--- CryptoLive/CryptoLive/src/main/java/com/cryptolive/CryptoLiveApplication.java
+++ CryptoLive/CryptoLive/src/main/java/com/cryptolive/CryptoLiveApplication.java
@@ -1,4 +1,4 @@
-package com.cryptolive.cryptolive;
+package com.cryptolive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- CryptoLive/CryptoLive/src/test/java/com/cryptolive/CryptoLiveApplicationTests.java
+++ CryptoLive/CryptoLive/src/test/java/com/cryptolive/CryptoLiveApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cryptolive.cryptolive;
+package com.cryptolive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
