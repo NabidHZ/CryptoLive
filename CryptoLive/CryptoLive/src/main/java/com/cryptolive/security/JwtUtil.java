edb2eead492c667c19b7a6cdf13146f7efb3e20e
Nahaza2425 <nabidhassan34@gmail.com>
2025-06-09 19:00:50 +0200

    OAuth y JWT, registro y login funcionando

--- CryptoLive/CryptoLive/src/main/java/com/cryptolive/security/JwtUtil.java
+++ CryptoLive/CryptoLive/src/main/java/com/cryptolive/security/JwtUtil.java
@@ -2,6 +2,7 @@ package com.cryptolive.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
@@ -9,7 +10,9 @@ import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String jwtSecret = System.getenv("JWT_SECRET");
+    @Value("${JWT_SECRET}")
+    private String jwtSecret;
+
     private final long jwtExpirationMs = 86400000; // 1 día
 
     private Key getSigningKey() {
