59b4c25bf6d713392e4645e143f52812ec0187b0
NabidHZ <nabidhassan34@gmail.com>
2025-08-09 20:24:33 +0200

    Lamada a las 20 monedas con mas valor desde el front al back

--- CryptoLive/CryptoLive/src/main/java/com/cryptolive/controller/HomeController.java
+++ CryptoLive/CryptoLive/src/main/java/com/cryptolive/controller/HomeController.java
@@ -1,13 +1,21 @@
 // src/main/java/com/cryptolive/controller/HomeController.java
 package com.cryptolive.controller;
 
+import com.cryptolive.service.CryptoMarketService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private CryptoMarketService cryptoMarketService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("top20", cryptoMarketService.getTop20Coins());
         return "index";
     }
 }
